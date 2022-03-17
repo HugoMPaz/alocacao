2cb274929794e3d8ba1e140b3b5468f87e7814f9
Hugo <hugompaz17@gmail.com>
2022-03-17 19:40:13 -0300

    alocacao

---
+++ alocacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    char *vnome;
+    vnome = (char *) malloc(sizeof(char));
+    gets(vnome);
+    cout << vnome << endl;
+    return 0;
+}
